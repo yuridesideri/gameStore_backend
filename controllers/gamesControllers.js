30322903b97ad96865e35fd97ae27857d00d6024
Yuri Desideri <yuridesideri12@gmail.com>
2022-12-08 16:09:04 -0300

    feat: games get & post

--- controllers/gamesControllers.js
+++ controllers/gamesControllers.js
@@ -1,10 +1,38 @@
-import connection, { gamesTb } from "../database/database.js";
+import connection, { categoriesTb, gamesTb } from "../database/database.js";
+import { gameSchema } from "../models/gamesModel.js";
 
 export async function listGames (req, res){
      try{
         const reqParams = req.params;
-        await connection.query(`SELECT * FROM ${gamesTb}`);
+        //TODO
+        //Adicionar filtro de queryString
+        const games = await connection.query(`SELECT * FROM $1`, [gamesTb]);
+        //TODO
+        //Adicionar parte que pega o categoryId e concatena no objeto a categoryName
+        res.send(games);
      } catch (err){
         res.send('generic error');
      }
+}
+
+
+export async function postGames (req, res){
+    const gameToPost = req.body;
+    try{
+        const gameToPostValid = await gameSchema.validateAsync(gameToPost);
+        const {categoryId, name} = gameToPostValid;
+        const existCategoryId = await connection.query(`SELECT id FROM $1 WHERE id='$2'`,[categoriesTb, categoryId]);
+        const existName = await connection.query(`SELECT name FROM $1 WHERE name = '$2'`,[gamesTb, name])
+        if(!existCategoryId){
+            throw new Error('Category not Found');
+        }
+        if(!existName){
+            throw new Error('Game name already exists');
+        }
+    } catch (err){
+        res.send('generic error');
+        //TODO
+        //Retorna err 400 (category not found)
+        //Retornar err 409 (game exists)
+    }
 }
--- routes/routeIndex.js
+++ routes/routeIndex.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import categoriesRouter from "./categoriesRoute.js";
+import gamesRouter from "./gamesRoute.js";
+import clientsRouter from "./clientsRoute.js";
+import rentRoutes from "./rentRoute.js";
 
 const router = Router();
 
 router.use(categoriesRouter);
-
+router.use(gamesRouter);
+router.use(clientsRouter);
+router.use(rentRoutes);
 
 
 
