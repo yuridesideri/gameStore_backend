43e6d8835c2fad52143267041b0ae1cc499c6658
Yuri Desideri <yuridesideri12@gmail.com>
2022-12-13 16:02:30 -0300

    feat: error status code for customers

--- controllers/customersControllers.js
+++ controllers/customersControllers.js
@@ -4,18 +4,25 @@ import { insertCustomerSchema } from "../models/customersModel.js";
 
 
 
-
-
-
-
 export async function listCustomers(req, res){
-    const queryString = req.params;
+    let {cpf = ""} = req.query;
+    const {id = null} = req.params;
+    if (!id) cpf = `${cpf}%`;
+
     try{
-        //TODO
-        //Filtro por CPF
-        const {rows: customers} = await connection.query(`SELECT * FROM ${customersTb}`);
+        const {rows: customers} = await connection.query(`
+        SELECT * 
+        FROM ${customersTb}
+        WHERE (cpf LIKE $1) OR (id = $2)
+        
+        `,[cpf, id])
+
+        if (customers.length === 0 && id) throw new Error("This id is inexistent")
+
         res.send(customers);
     } catch (err){
+        if (err.message === "This id is inexistent") res.status(404);
+        else res.status(400)
         res.send(err);
     }
 }
@@ -25,17 +32,21 @@ export async function insertCustomers (req, res){
     const customerToInsert = req.body;
     try{
         const customerToInsertValid = await insertCustomerSchema.validateAsync(customerToInsert);
-        // const {name, phone, cpf, birthday} = customerToInsertValid;
-        //TODO 
-        //Check if cpf already exists
-        console.log(customerToInsertValid);
+        const {rows: [{cpf: cpfAlreadyExists}]} = await connection.query(`
+            SELECT cpf
+            FROM ${customersTb} 
+            WHERE cpf = $1
+        `, customerToInsertValid.cpf)
+
+        if (cpfAlreadyExists) throw new Error("This CPF is already in use");
+
+
         await InsertQuery(customersTb, customerToInsertValid);
         res.sendStatus(201);
     } catch(err){
-        console.log(err);
+        if (err.message === "This CPF is already in use") res.status(409)
+        else res.status(400);
         res.send(err);
-        //TODO
-        //If cpf exists -> 409
     }
 }
 
@@ -46,15 +57,21 @@ export async function updateCustomer (req, res){
     try{
         const customerToUpdateValid = await insertCustomerSchema.validateAsync(customerDataToUpdate);
         const {name, phone, cpf, birthday} = customerToUpdateValid;
-        console.log(customerToUpdateValid);
-        //TODO
-        //Check if CPF Exists
+
+        const {rows:[{cpf:cpfAlreadyExists}]} = await connection.query(`
+            SELECT * 
+            FROM ${customersTb}
+            WHERE cpf = $1
+        `,[cpf]);
+        if (cpfAlreadyExists) throw new Error("This CPF is already in use");
         await connection.query(`UPDATE ${customersTb} SET (name, phone, cpf, birthday) = ($1, $2, $3, $4) WHERE id=$5`,[name, phone,cpf,birthday, customerIdToUpdate]);
+        
         res.sendStatus(200);
     } catch (err){
         res.send(err);
-        // TODO
-        // schema ==> 400
-        // cpf exists ==> 409
+
+        if (err.message === "This CPF is already in use") res.status(409);
+        else res.status(400);
+        res.send(err);
     }
 }
